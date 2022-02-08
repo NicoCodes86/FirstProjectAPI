da76581d437ce8d5a051eeb98c11e150210ed607
NicoCodes86 <paolocamacho423@gmail.com>
2022-02-07 20:06:35 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+function weatherFinder() {
+
+
+    import { cityFinder } from "./cityFinder.js";
+    console.log(cityFinder); 
+    
+    cityFinder(); 
+
+    
+
+    console.log("whats up")
+
+
+
+
+}
